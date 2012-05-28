c20f895dd18400c228a5e91b3d0cd3747a00258e
Vasant <vasant.v@gmail.com>
2012-05-28 10:18:13 +0530

    Adding geocode..pl
    Basic version of geocode.pl

---
+++ geocode.pl
@@ -0,0 +1,39 @@
+use Google::GeoCoder::Smart;
+use Time::localtime;
+$geo = Google::GeoCoder::Smart->new();
+$tm = localtime;
+
+# reading inputs from a file 
+open (ADDFILE,'<addressfile.txt');
+
+# change CRLF for windows
+$counter = 1;
+while(<ADDFILE>){
+	chomp;
+
+	$filename = "resfile".$tm->mday."-".$tm->mon.".txt";
+	open (RESFILE,">>$filename");
+	
+	# read line from file 
+	my ($id,$add,$city,$state,$zip) = split (",");
+	my ($resultnum, $error, @results, $returncontent) = $geo->geocode("address" => $add, "city"=> $city, "state"=> $state, "zip"=> $zip);
+	$lat = $results[0]{geometry}{location}{lat};
+	$lng = $results[0]{geometry}{location}{lng};
+		
+	if($lat && $lng) {
+	# printing to result file
+	print RESFILE "$id,$add,$city,$state,$zip,$lat,$lng\n";
+	#print $counter."\n";	
+	}
+	else
+	{ print RESFILE "$id,$add,$city,$state,$zip,ERR,$error\n"; 
+	  #print $counter."$error Error!\n";
+	}
+	$counter++;
+	sleep(2);
+	if($counter %2400 == 0) {
+		close (RESFILE);
+		sleep (24*60*60*1000); 
+	}
+}
+close (ADDFILE);
